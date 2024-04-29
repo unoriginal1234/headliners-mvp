382ac7857b085d37df1ad931f9637b1388ba513f
unoriginal1234 <eric.t.roth@gmail.com>
2024-04-29 10:43:09 -0700

    Connect to db with connection string

--- db/index.js
+++ db/index.js
@@ -1,13 +1,21 @@
 require('dotenv').config()
 const {Client} = require('pg');
 
+// function clientConnect() {
+//   return new Client({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     database: process.env.DB_NAME,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PW,
+//   })
+// };
+
+const connectionString = process.env.CONNECTION_STRING;
+
 function clientConnect() {
   return new Client({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
+    connectionString
   })
 };
 
