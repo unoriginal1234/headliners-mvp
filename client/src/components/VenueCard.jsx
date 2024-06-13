6909d54e444fe4827ead779294379d9fbdf003a7
unoriginal1234 <eric.t.roth@gmail.com>
2024-06-13 16:40:35 -0700

    Readable date

--- client/src/components/VenueCard.jsx
+++ client/src/components/VenueCard.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 
-
-
-
 const VenueCard = ({venue}) => {
-
-
 
   console.log(venue.Date, 'this is the date of the show')
 
-  // console.log(format(new Date(venue.Date), "EEEE, MMMM dd yyyy"));
+  const months = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+  };
+
+  const showDate = venue.Date.split('-')
+  const readableShowDate = months[parseInt(showDate[1])] + ' ' + showDate[2] + ',' + showDate[0]
 
   return (
     <>
@@ -17,7 +28,7 @@ const VenueCard = ({venue}) => {
       <p className="venue-name">{venue.Name}</p>
 
       <div className="venue-setting">
-        <p>Date: {venue.Date}</p>
+        <p>Date: {readableShowDate}</p>
         <p>Location: {venue.Location}</p>
       </div>
     </>
