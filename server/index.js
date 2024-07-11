402ef849d33bd5fd955ddb9fceb8bda87fe53181
unoriginal1234 <eric.t.roth@gmail.com>
2024-07-11 16:48:31 -0700

    Serve public folder

--- server/index.js
+++ server/index.js
@@ -2,16 +2,22 @@ require('dotenv').config()
 const express = require('express');
 const getCard = require('../db/models').getCard
 const getArtists = require('../db/models').getArtists
+// const myLogger = require('./middleware/visitCounter')
 
 const app = express();
 const PORT = process.env.PORT;
 
 const path = require('path');
 
+// app.use(myLogger)
+
 app.get('/game', getCard);
 app.get('/artists', getArtists);
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
